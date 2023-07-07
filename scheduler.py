6bb0b3ce1b48bbc85d82ef452ce3515ae99e9bc2
Vladimir <vkrybnikov@gmail.com>
2023-07-07 16:23:30 +0300

    local scheduler fix

--- scheduler.py
+++ scheduler.py
@@ -9,7 +9,7 @@ def run_script(script):
         # get the current environment variables
         env_vars = os.environ.copy()
         # run the script with the current environment variables
-        subprocess.run(['python', script], check=True, env=env_vars)
+        subprocess.run(['/usr/local/bin/python3', script], check=True, env=env_vars)
 
     except Exception as e:
         print(f"Error occurred while running {script}: {str(e)}")
@@ -24,10 +24,10 @@ def form_ai_posts():
 def telegram_bot():
     run_script('telegram_bot.py')
 
-schedule.every().day.at("16:10").do(fetch_articles)
-schedule.every().day.at("16:25").do(form_ai_posts)
-schedule.every().day.at("17:00").do(telegram_bot)
-schedule.every().day.at("10:10").do(telegram_bot)
+schedule.every().day.at("16:12").do(fetch_articles)
+schedule.every().day.at("16:20").do(form_ai_posts)
+schedule.every().day.at("16:30").do(telegram_bot)
+schedule.every().day.at("16:40").do(telegram_bot)
 
 while True:
     schedule.run_pending()
