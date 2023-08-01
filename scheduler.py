35cdefc8e4354f157980839b1575073004b50339
Vladimir <vkrybnikov@gmail.com>
2023-08-01 21:49:35 +0300

    schedule change

--- scheduler.py
+++ scheduler.py
@@ -31,7 +31,7 @@ schedule.every().day.at("10:30").do(fetch_articles)
 schedule.every().day.at("10:40").do(form_ai_posts)
 schedule.every().day.at("16:50").do(telegram_bot)
 schedule.every().day.at("10:58").do(telegram_bot)
-schedule.every().day.at("20:15").do(cleanup_db)
+schedule.every().day.at("09:15").do(cleanup_db)
 print('Jobs planned!')
 
 while True:
